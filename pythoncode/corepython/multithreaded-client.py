ebb7e07506d49d9e604e82cfd45a48c9bfa45e38
rahul-72 <charan7rahul@gmail.com>
2019-03-29 21:06:54 +0530

    core python is added

---
+++ pythoncode/corepython/multithreaded-client.py
@@ -0,0 +1,38 @@
+def send(client):
+	while True : 
+		msg  = input("server-->")
+		client.send(msg.encode())
+		if msg.lower().strip() == 'bye' :
+			client.close()
+			print("Connection closed by server")
+			break
+		
+def recv(client):
+	while True : 
+		msg = client.recv(1024).decode()
+		msg = f"client--> {msg}"
+		print(msg.rjust(50))
+		if msg.lower().strip() == 'bye' : 
+			client.close()
+			print("Connection closed by client")
+			break
+		
+import socket
+import threading
+server = socket.socket() 
+host = 'localhost'
+port = 12345
+server.connect((host,port))
+
+print(f"Connected to {host}:{port}")
+
+msg_send = threading.Thread(target=send,args=(server,))
+msg_recv = threading.Thread(target=recv,args=(server,))
+
+msg_send.start()
+msg_recv.start()
+
+msg_send.join()
+msg_recv.join() 			
+
+
